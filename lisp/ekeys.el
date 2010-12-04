94d2f4343877e78552dcf8afd0ddcb4adbcc8c7c
Matthew Stickney <fiendishlinux@gmail.com>
2010-12-04 11:34:09 -0500

    Add org-mode config

--- lisp/ekeys.el
+++ lisp/ekeys.el
@@ -3,3 +3,8 @@
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
+
+;; Org-mode stuff
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
