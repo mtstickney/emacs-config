200b25e62cc03c013e49b251e544caf37d6413a8
Matthew Stickney <fiendishlinux@gmail.com>
2011-02-04 18:29:32 -0500

    Update config to run on windows.
    Added OS predicate, set up lisp and nxml code appropriately based on OS.

--- lisp/slime-config.el
+++ lisp/slime-config.el
@@ -1,3 +1,8 @@
-(require 'slime)
-(setq inferior-lisp-program "/usr/bin/sbcl")
-(slime-setup '(slime-repl))
+(if (on-windows)
+    (progn
+      (setq inferior-lisp-program "clisp.exe")
+      (load "C:\\quicklisp\\slime-helper.el"))
+  (progn
+    (require 'slime)
+    (setq inferior-lisp-program "/usr/bin/sbcl")
+    (slime-setup '(slime-repl))))
