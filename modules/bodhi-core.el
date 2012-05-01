671d76cc1af147f3040a2700194048ff26cad955
Matthew Stickney <fiendishlinux@gmail.com>
2012-05-01 13:38:58 -0400

    Reindent

--- modules/bodhi-core.el
+++ modules/bodhi-core.el
@@ -94,33 +94,34 @@
   "Rename both the current buffer and the file it's visiting to NEW_NAME."
   (interactive "sNew name:")
   (let ((name (buffer-name))
-	(filename (buffer-file-name)))
+        (filename (buffer-file-name)))
     (if (not filename)
-	(message "Buffer '%s' is not visiting a file!" name))
+        (message "Buffer '%s' is not visiting a file!" name))
     (if (get-buffer new-name)
-	(message "A buffer named '%s' already exists!" new-name)
+        (message "A buffer named '%s' already exists!" new-name)
       (progn (rename-file name new-name 1)
-	     (rename-buffer new-name)
-	     (set-visited-file-name new-name)
-	     (set-buffer-modified-p nil)))))
+             (rename-buffer new-name)
+             (set-visited-file-name new-name)
+             (set-buffer-modified-p nil)))))
 
 ;; move buffer and file to another dir
 (defun move-buffer-file (dir)
   "Move both current buffer and file it's visiting to DIR."
   (interactive "DNew directory: ")
   (let* ((name (buffer-name))
-	 (filename (buffer-file-name))
-	 (dir
-	  (if (string-match dir "\\(?:/\\|\\\\)$")
-	      (substring dir 0 -1) dir))
-	 (new-name (concat dir "/" name)))
+         (filename (buffer-file-name))
+         (dir
+          (if (string-match dir "\\(?:/\\|\\\\)$")
+              (substring dir 0 -1) dir))
+         (new-name (concat dir "/" name)))
     (if (not filename)
-	(message "Buffer '%s' is not visiting a file!" name)
+        (message "Buffer '%s' is not visiting a file!" name)
       (progn
-	(copy-file filename newname 1)
-	(delete-file filename)
-	(set-visited-file-name newname)
-	(set-buffer-modified-p nil) t))))
+        (copy-file filename newname 1)
+        (delete-file filename)
+        (set-visited-file-name newname)
+        (set-buffer-modified-p nil) t))))
+
 (defun bodhi-delete-file-and-buffer ()
   "Kills the current buffer and deletes the file it is visiting."
   (interactive)
