5aa00bcc647c019965c834123260552b2c5313de
Matthew Stickney <fiendishlinux@gmail.com>
2012-05-01 13:30:55 -0400

    Alias ack-and-a-half commands

--- modules/bodhi-editor.el
+++ modules/bodhi-editor.el
@@ -156,4 +156,10 @@
     ;; otherwise fallback to helm-mini
     (helm-mini)))
 
+;; shorter ack-and-a-half aliases
+(defalias 'ack 'ack-and-a-half)
+(defalias 'ack-same 'ack-and-a-half-same)
+(defalias 'ack-find-file 'ack-and-a-half-find-file)
+(defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
+
 (provide 'bodhi-editor)
