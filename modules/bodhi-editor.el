38fd0e509b9fde372254a9d12a85c0778ad1ae42
Matthew Stickney <fiendishlinux@gmail.com>
2012-05-01 13:27:50 -0400

    Save buffers on buffer/file switch

--- modules/bodhi-editor.el
+++ modules/bodhi-editor.el
@@ -61,4 +61,19 @@
 (savehist-mode t)
 
 
+;; save (file) buffers on window/buffer switch
+(defadvice switch-to-buffer (before save-buffer-now activate)
+  (when (buffer-file-name) (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+  (when (buffer-file-name (save-buffer))))
+(defadvice windmove-up (before other-window-now activate)
+  (when (buffer-file-name (save-buffer))))
+(defadvice windmove-down (before other-window-now activate)
+  (when (buffer-file-name) (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+  (when (buffer-file-name) (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+  (when (buffer-file-name) (save-buffer)))
+
+
 (provide 'bodhi-editor)
