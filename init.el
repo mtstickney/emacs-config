9c711d20e38ed5f414e1a578d226394c5b5c3228
Matthew Stickney <fiendishlinux@gmail.com>
2017-04-22 11:50:14 -0400

    Add auto-generated package.el line

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ;; Emacs24 Initialization file
 ;; The File Formerly Known As .emacs
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'cl)
 
 (message "Bodhi approaches... Patience, %s."
