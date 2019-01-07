d2862f369ca4e9d49f238df7927b82b9c9e7db6b
Matthew Stickney <fiendishlinux@gmail.com>
2019-01-06 19:26:53 -0500

    Fix sync dir for deft-mode.
    The old directory was from btsync, which has been superseded by Syncthing.
    The dropbox part should probably also be dropped, since we're doing that
    for org-mode files.

--- modules/bodhi-deft.el
+++ modules/bodhi-deft.el
@@ -1,10 +1,9 @@
-
 ;; Use org-mode as the text format
 (setq deft-extensions '("org" "txt" "text" "md" "markdown"))
 (setq deft-text-mode 'org-mode)
 (setq deft-directory (if (file-exists-p "~/Dropbox")
                         "~/Dropbox/notes"
-                      "~/sync/notes"))
+                      "~/Sync/notes"))
 ;; Don't use the timestamp-based default format. This is a) different
 ;; than all the other notes files we have, and b) broken on windows
 ;; because a colon in a filename denotes an alternate NTFS stream.
