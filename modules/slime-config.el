0e96ea3e81b093c2f9739d6665e1db5f9c197d7c
Matthew Stickney <fiendishlinux@gmail.com>
2019-01-06 19:24:12 -0500

    Remove old config files.
    These were part of the pre-emacs-24 config we had, which should have been
    removed after they were ported to the new structure. Dunno why that didn't
    happen, or how I hadn't noticed they were still kicking around. Situation
    rectified.

--- modules/slime-config.el
+++
@@ -1,8 +0,0 @@
-;; NOTE: the slime-helper.el does autoloads on its own
-(if *on-windows*
-    (progn
-      (setq inferior-lisp-program "clisp.exe")
-      (load "C:\\quicklisp\\slime-helper.el"))
-  (progn
-    (setq inferior-lisp-program "/usr/bin/sbcl")
-    (load (expand-file-name "~/quicklisp/slime-helper.el"))))
