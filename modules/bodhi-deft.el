212d233290a1be6939d9f76144f2645ebc18afaf
Matthew Stickney <fiendishlinux@gmail.com>
2020-07-31 12:51:38 -0400

    Update notes directory.
    The sync folder structure has been changed, update the notes dir to match.

--- modules/bodhi-deft.el
+++ modules/bodhi-deft.el
@@ -1,7 +1,7 @@
 ;; Use org-mode as the text format
 (setq deft-extensions '("org" "txt" "text" "md" "markdown"))
 (setq deft-text-mode 'org-mode)
-(setq deft-directory "~/Sync/notes")
+(setq deft-directory "~/Sync/core/notes")
 ;; Don't use the timestamp-based default format. This is a) different
 ;; than all the other notes files we have, and b) broken on windows
 ;; because a colon in a filename denotes an alternate NTFS stream.
